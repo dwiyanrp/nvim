c988c3ce308e2ca08146835147b3913f4e78742a
Dwiyan Rezkia Putra <dwiyanrp@gmail.com>
2024-12-23 11:39:21 +0700

    custom gopls in lspconfig & run gofmt on save
    https://github.com/ray-x/go.nvim?tab=readme-ov-file#format-on-save

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,16 +4,31 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls", "gopls" }
+local servers = {
+  html = {},
+  cssls = {},
+  gopls = {
+    cmd = {"gopls"},
+    filetypes = { "go", "gomod", "gowork", "gotmpl" },
+    settings = {
+      gopls = {
+        completeUnimported = true,
+        usePlaceholders = true,
+        analyses = {
+          unusedparams = true,
+        },
+      },
+    },
+  },
+}
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
-for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
-    on_init = nvlsp.on_init,
-    capabilities = nvlsp.capabilities,
-  }
+for name, opts in pairs(servers) do
+  opts.on_attach = nvlsp.on_attach
+  opts.on_init = nvlsp.on_init
+  opts.capabilities = nvlsp.capabilities
+  lspconfig[name].setup(opts)
 end
 
 -- configuring single server, example: typescript
